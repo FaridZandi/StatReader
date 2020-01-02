947e584ee390cf3a57b4bf7b7ea17716b7563443
Farid Zandi <Faridzs76@gmail.com>
2020-01-02 13:41:52 +0330

    fixing static

--- StatReader/settings.py
+++ StatReader/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +26,7 @@ SECRET_KEY = '1glc)jzeiorvp0j%cbxe0)$78vfrf06k*j$x2!6k80rc7mrbhu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['statreader.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'statreader.herokuapp.com']
 
 # Application definition
 
@@ -124,8 +126,9 @@ STATICFILES_DIRS = (
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 
+import django_heroku
+django_heroku.settings(locals())
