1058d40e6816e6ac5024e2172e287a06f4658572
Farid Zandi <Faridzs76@gmail.com>
2020-01-02 17:13:35 +0330

    fixing static

--- StatReader/settings.py
+++ StatReader/settings.py
@@ -117,9 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 )
 
 print(STATICFILES_DIRS)
