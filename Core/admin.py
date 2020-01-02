1a99113b01067553aa66a7997689e50fa7e16ae0
Farid Zandi <Faridzs76@gmail.com>
2020-01-02 14:08:50 +0330

    adding Stat model

--- Core/admin.py
+++ Core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from Core.models import Stat
+
 
 # Register your models here.
+
+class StatAdmin(admin.ModelAdmin):
+    list_display = ("id", "url", "query_selector", "creator")
+
+    class Meta:
+        model = Stat
+
+admin.site.register(Stat, StatAdmin)
+
--- Core/models.py
+++ Core/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+
+class Stat(models.Model):
+    url = models.URLField()
+
+    query_selector = models.CharField(max_length=500)
+
+    creator = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+
+    subscribers = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                         related_name="subscribed_stats")
