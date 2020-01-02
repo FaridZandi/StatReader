cbd310bebc6c1c1f7e7a1cfb4bd53479dc462dc3
Farid Zandi <Faridzs76@gmail.com>
2020-01-02 15:00:03 +0330

    adding paramiko

--- Core/views.py
+++ Core/views.py
@@ -7,7 +7,7 @@ from django.views import View
 import subprocess
 import sys
 
-from paramiko import SSHClient
+from paramiko import SSHClient, AutoAddPolicy
 
 
 class TestView(View):
@@ -15,6 +15,8 @@ class TestView(View):
     def get(self, request, *args, **kwargs):
         command = "cd farid/seleniumtest; python3.6 test.py"
         client = SSHClient()
+
+        client.set_missing_host_key_policy(AutoAddPolicy())
         client.load_system_host_keys()
         client.connect('213.233.180.18', port=10147, username="amirhossein", password="frisbe")
         stdin, stdout, stderr = client.exec_command(command)
